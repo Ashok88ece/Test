912b2ca5e1486b37f68d2bb8a7f04083eb05466a
Ashok88ece <108461311+Ashok88ece@users.noreply.github.com>
2026-06-16 16:38:09 +0530

    Add greeting message for Ashok
    Test Message

--- sample.java
+++ sample.java
@@ -6,6 +6,7 @@ public class Hello{
     System.out.println("Hello Test3");
     System.out.println("Hello Test4");
     System.out.println("Hello");
+    System.out.println("Hello Ashok How are You");
     
   }
 }
